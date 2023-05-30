418ec96d652e88b8854df2ba65163b47170ebbad
silence48 <35656692+silence48@users.noreply.github.com>
2023-05-30 19:49:26 -0400

    update to support v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,11 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "../src/preset.js"
-  ],
-  "framework": "@storybook/react"
-}
+  "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "../src/preset.js", "@storybook/addon-mdx-gfm"],
+  "framework": {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
